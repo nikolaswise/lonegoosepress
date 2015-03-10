d684902c62ab608d16d0d7fdfe822b9155843778
Nikolas Wise <nikolaswise@gmail.com>
2015-03-10 12:34:57 -0700

    cart tweaks

--- www/assets/js/cart.js
+++ www/assets/js/cart.js
@@ -5,10 +5,15 @@
 
 var myApp = angular.module('myApp',['ngCart']);
 
-myApp.controller ('myCtrl', ['ngCart', function(ngCart) {
+myApp.controller ('myCtrl', ['$scope', '$http', 'ngCart', function($scope, $http, ngCart) {
     ngCart.setTax(7.5);
     ngCart.setShipping(2.99);
-    console.log (ngCart);
+    // console.log (ngCart);
+
+    $scope.checkout = function() {
+      console.log(ngCart.totalCost()*100)
+     //$http.post('cart/', ngCart.totalCost()*100);
+    };
 }]);
 
-console.log(angular);
+// console.log(angular);
