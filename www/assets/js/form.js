225562b77b2cce4a84a8bf3bf43c9dffec807444
Nikolas Wise <nikolaswise@gmail.com>
2015-05-03 12:08:59 -0700

    small refresher

--- www/assets/js/form.js
+++ www/assets/js/form.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
-Stripe.setPublishableKey("pk_test_6chGmDIc3ftpiOH7C5bQl6pW");
+Stripe.setPublishableKey("pk_live_wLZlZRJUMFZTeyK5tab54l8X");
 
 var stripeResponseHandler = function stripeResponseHandler(status, response) {
   var $form = $("#payment-form");
