2159ca72300fa5d1d7a6bb708b4b3fa0fbe1677b
Nikolas Wise <nikolaswise@gmail.com>
2016-05-11 09:17:06 -0700

    update deps

--- build/assets/js/bundle.js
+++ build/assets/js/bundle.js
@@ -537,6 +537,9 @@ var currentQueue;
 var queueIndex = -1;
 
 function cleanUpNextTick() {
+    if (!draining || !currentQueue) {
+        return;
+    }
     draining = false;
     if (currentQueue.length) {
         queue = currentQueue.concat(queue);
