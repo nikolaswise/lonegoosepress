6d4883136406ba9a61968a292c7706f90b2ecec2
nikolaswise <nikolaswise@gmail.com>
2017-08-23 10:39:43 -0700

    therrrrrrrrr we go

--- source/_helpers/catalog.js
+++ source/_helpers/catalog.js
@@ -33,7 +33,91 @@ module.exports = function (site, cb) {
       data: {project: project}
     })
   }
+  function buildBroadside (project) {
+    site.push({
+      title: project.title,
+      template: '_templates/project.html',
+      block: 'project',
+      content: '',
+      file: source + urlify(project.title),
+      dest: './build/publications/broadsides/' + urlify(project.title) + '/index.html',
+      url: '/' + urlify(project.title) + '/',
+      root: '../..',
+      isMarkdown: false,
+      project: project,
+      data: {project: project}
+    })
+  }
+  function buildEphemera (project) {
+    site.push({
+      title: project.title,
+      template: '_templates/project.html',
+      block: 'project',
+      content: '',
+      file: source + urlify(project.title),
+      dest: './build/publications/ephemera/' + urlify(project.title) + '/index.html',
+      url: '/' + urlify(project.title) + '/',
+      root: '../..',
+      isMarkdown: false,
+      project: project,
+      data: {project: project}
+    })
+  }
+
+  function buildDesign (project) {
+    site.push({
+      title: project.title,
+      template: '_templates/project.html',
+      block: 'project',
+      content: '',
+      file: source + urlify(project.title),
+      dest: './build/commisions/design/' + urlify(project.title) + '/index.html',
+      url: '/' + urlify(project.title) + '/',
+      root: '../..',
+      isMarkdown: false,
+      project: project,
+      data: {project: project}
+    })
+  }
+
+  function buildSupport (project) {
+    site.push({
+      title: project.title,
+      template: '_templates/project.html',
+      block: 'project',
+      content: '',
+      file: source + urlify(project.title),
+      dest: './build/commisions/publishing/' + urlify(project.title) + '/index.html',
+      url: '/' + urlify(project.title) + '/',
+      root: '../..',
+      isMarkdown: false,
+      project: project,
+      data: {project: project}
+    })
+  }
+
+  function buildRealization (project) {
+    site.push({
+      title: project.title,
+      template: '_templates/project.html',
+      block: 'project',
+      content: '',
+      file: source + urlify(project.title),
+      dest: './build/commisions/realization/' + urlify(project.title) + '/index.html',
+      url: '/' + urlify(project.title) + '/',
+      root: '../..',
+      isMarkdown: false,
+      project: project,
+      data: {project: project}
+    })
+  }
   catalog.books.map(buildBook)
+  catalog.broadsides.map(buildBroadside)
+  catalog.ephemera.map(buildEphemera)
+  catalog.design.map(buildDesign)
+  catalog.publishing.map(buildSupport)
+  catalog.realization.map(buildRealization)
+
   cb(null, site)
 }
 
