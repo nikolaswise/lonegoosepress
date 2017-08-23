af43733a652a77ab3d4a36b68897f491952fd047
nikolaswise <nikolaswise@gmail.com>
2017-08-23 10:29:57 -0700

    parse dat yaml

--- source/_helpers/catalog.js
+++ source/_helpers/catalog.js
@@ -10,6 +10,15 @@ urlify = require('urlify').create({
 
 module.exports = function (site, cb) {
   console.log('this is the site')
+  var source = path.resolve('./', 'source')
+  var target = path.join(source, 'catalog.yml')
+  var catalog
+  try {
+    catalog = yaml.safeLoad(fs.readFileSync(target, 'utf8'));
+  } catch (e) {
+    console.error(e);
+  }
+  console.log(catalog)
   cb(null, site)
 }
 
