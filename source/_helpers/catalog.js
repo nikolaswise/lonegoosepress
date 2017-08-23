ffd599d37b1a72b73ec5592f1e476505f8681640
nikolaswise <nikolaswise@gmail.com>
2017-08-23 10:27:57 -0700

    reqs in catalog

--- source/_helpers/catalog.js
+++ source/_helpers/catalog.js
@@ -1,3 +1,13 @@
+yaml = require('js-yaml');
+fs   = require('fs');
+path = require('path');
+urlify = require('urlify').create({
+  toLower: true,
+  spaces:"-",
+  nonPrintable:"",
+  trim:true
+});
+
 module.exports = function (site, cb) {
   console.log('this is the site')
   cb(null, site)
