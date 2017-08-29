0f03f6de68bfbc6e075ee544d3451395c804c1e7
nikolaswise <nikolaswise@gmail.com>
2017-08-29 09:09:07 -0700

    remove old stuff

--- source/_assets/js/index.js
+++ source/_assets/js/index.js
@@ -1,1 +1,7 @@
+import bus from 'modular-bus'
+import * as classy from 'modular-class'
+import * as aria from 'modular-aria'
+import * as dom from 'modular-dom'
+import * as event from 'modular-event'
+
 console.log('WELP')
