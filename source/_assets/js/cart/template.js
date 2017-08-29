d8a7a6c2150081176755585895d350657e18a0dd
nikolaswise <nikolaswise@gmail.com>
2017-08-29 13:30:51 -0700

    opening the mouth of the dead

--- source/_assets/js/cart/template.js
+++ source/_assets/js/cart/template.js
@@ -1,6 +1,6 @@
 const lineItem = item => {
   return`
-    <div class="first-column font-size-1 padding-tailer-half trailer-half column-11 phone-column-5">
+    <div class="first-column font-size-1 padding-tailer-half trailer-half column-17 phone-column-5">
       <a href="#" class="js-remove-one gutter-right-1 link-white" data-id="${item.id}" data-add="false">-</a>
       <span>${item.num}</span>
       <a href="#" class="js-add-one gutter-left-1 link-white" data-id="${item.id}" data-add="true">+</a>
@@ -16,13 +16,12 @@ const render = cart => {
   if (cart.items.length == 0) {
     return `
       <p class="text-center padding-leader-2 padding-trailer-2">No items in your cart.</p>
-      <a href="#" class="btn right js-modal-toggle" data-modal="cart">Keep Shopping</a>
     `
   }
   return `
     <h3 class="trailer-1">Your Cart</h3>
     ${cart.items.map(item => lineItem(item)).join('')}
-    <div class="column-11 phone-column-5">
+    <div class="column-17 phone-column-5">
       <p class="font-size-1 trailer-half text-right gutter-right-1">Subtotal: $ ${ cart.subtotal }</p>
       <p class="font-size-1 trailer-half text-right gutter-right-1">Shipping: $ ${ cart.shipping }</p>
       <p class="font-size-1 trailer-half text-right gutter-right-1">Total: $ ${ cart.total }</p>
