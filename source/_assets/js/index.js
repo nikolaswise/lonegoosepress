362a2826b017d8b507ed6b31dd777f09c5413990
nikolaswise <nikolaswise@gmail.com>
2017-08-29 12:51:27 -0700

    menu working agian simply

--- source/_assets/js/index.js
+++ source/_assets/js/index.js
@@ -20,3 +20,15 @@ setCartCounter()
 modal()
 
 
+let navToggles = dom.findElements('.js-expanding-toggle')
+let menus = dom.findElements('.js-expanding')
+const toggleMenu = () => {
+  console.log('tog tog tog')
+  menus.forEach(menu => {
+    classy.toggle(menu, 'is-active')
+  })
+}
+navToggles.forEach(toggle => {
+  event.add(toggle, 'click', toggleMenu)
+})
+
